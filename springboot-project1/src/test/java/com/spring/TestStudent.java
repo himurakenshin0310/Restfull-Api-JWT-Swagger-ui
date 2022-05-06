671d3f5a975a2b7396bc1359a8c91ff4884d94b3
kikyo <kikyo@host.docker.internal>
2022-05-06 11:27:49 +0700

    update

--- springboot-project1/src/test/java/com/spring/TestStudent.java
+++ springboot-project1/src/test/java/com/spring/TestStudent.java
@@ -1,19 +1,8 @@
 package com.spring;
 
-import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
 import javax.transaction.Transactional;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import com.spring.entity.Student;
-import com.spring.model.StudentDto;
-import com.spring.repository.StudentRepository;
-import com.spring.service.StudentService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,23 +10,23 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional
 @Slf4j
 public class TestStudent {
-	@Autowired
-	StudentRepository studentRepo;
+//	@Autowired
+//	StudentRepository studentRepo;
+//
+//	@Autowired
+//	StudentService studentService;
 
-	@Autowired
-	StudentService studentService;
-
-	@Test
-	public void testingInsertAndGetListAndRemoveAndUpdateStudent() throws ParseException {
-		Student student = new Student();
-		student.setName("testStudent");
-		student.setBirthday(new Date(new SimpleDateFormat("dd/MM/yyyy").parse("14/05/2022").getTime()));
-		studentService.save(StudentDto.from(student));
-		log.info("insert new student");
-		log.info("danh sach {}", studentRepo.findAll().size());
-		log.info("get list student");
-		studentService.deleteById(1l);
-		log.info("delete student");
-	}
+//	@Test
+//	public void testingInsertAndGetListAndRemoveAndUpdateStudent() throws ParseException {
+//		Student student = new Student();
+//		student.setName("testStudent");
+//		student.setBirthday(new Date(new SimpleDateFormat("dd/MM/yyyy").parse("14/05/2022").getTime()));
+//		studentService.save(StudentDto.from(student));
+//		log.info("insert new student");
+//		log.info("danh sach {}", studentRepo.findAll().size());
+//		log.info("get list student");
+//		studentService.deleteById(1l);
+//		log.info("delete student");
+//	}
 
 }
--- springboot-project1/src/test/java/com/spring/TestTeacher.java
+++ springboot-project1/src/test/java/com/spring/TestTeacher.java
@@ -1,18 +1,8 @@
 package com.spring;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
-
 import javax.transaction.Transactional;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import com.spring.entity.Teacher;
-import com.spring.model.TeacherDto;
-import com.spring.repository.TeacherRepository;
-import com.spring.service.TeacherService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,24 +11,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TestTeacher {
 
-	@Autowired
-	TeacherService teacherService;
-
-	@Autowired
-	TeacherRepository teacherRepo;
-
-	@Test
-	public void testingInsertAndGetListAndRemoveAndUpsertTeacher() throws Exception {
-		Teacher teacher = new Teacher();
-		teacher.setBrithdate(new Date(new SimpleDateFormat("dd/MM/yyyy").parse("14/05/2022").getTime()));
-		teacher.setName("teacher test");
-		teacherService.save(TeacherDto.from(teacher));
-		log.info("insert new teacher");
-		for (Teacher t : teacherRepo.findAll())
-			log.info("test teacher: teacher exist now have id : {}", t.getId());
-//		log.info("get list of teacher {}", teacherRepo.findAll().size());		teacherService.upsert(teacherService.findById(1l));
-		log.info("update teacher");
-		teacherService.deleteById(1l);
-		log.info("remove teacher");
-	}
+//	@Autowired
+//	TeacherService teacherService;
+//
+//	@Autowired
+//	TeacherRepository teacherRepo;
+//
+//	@Test
+//	public void testingInsertAndGetListAndRemoveAndUpsertTeacher() throws Exception {
+//		Teacher teacher = new Teacher();
+//		teacher.setBrithdate(new Date(new SimpleDateFormat("dd/MM/yyyy").parse("14/05/2022").getTime()));
+//		teacher.setName("teacher test");
+//		teacherService.save(TeacherDto.from(teacher));
+//		log.info("insert new teacher");
+//		for (Teacher t : teacherRepo.findAll())
+//			log.info("test teacher: teacher exist now have id : {}", t.getId());
+////		log.info("get list of teacher {}", teacherRepo.findAll().size());		teacherService.upsert(teacherService.findById(1l));
+//		log.info("update teacher");
+//		teacherService.deleteById(1l);
+//		log.info("remove teacher");
+//	}
 }
